1e7cfd6429bf6a74bc79bee6bb12fc9e6d9121aa
JD Russo <thejesusrambo@gmail.com>
2014-07-16 17:35:15 -0700

    Initial commit of XML parser

---
+++ xmltosql.py
@@ -0,0 +1,135 @@
+import psycopg2 as sql
+import xml.etree.ElementTree as ET
+import re, sys
+
+class XMLParser():
+
+	def __init__(self, testResults="../nosetests.xml", dbname="results", dbuser='postgres', password='ender'):
+
+		self.conn = sql.connect("dbname=%s user=%s password=%s" % (dbname, dbuser, password))
+		self.cursor = self.conn.cursor()
+		print("Connected")
+
+		self.xmlTree = ET.parse(testResults)
+		self.xmlRoot = self.xmlTree.getroot()
+
+		self.conn.commit()
+
+	def parse(self):
+
+		run_num = 0
+
+		self.cursor.execute('SELECT MAX(run_num) FROM test_result;')
+
+		try:
+			run_num = self.cursor.fetchone()[0] + 1
+		except TypeError:
+			run_num = 0
+
+		print("Run_num is %d" % run_num)
+
+		for child in self.xmlRoot:
+
+			self.cursor.execute('SELECT MAX(test_result_id) FROM test_result;')
+			try:
+				id_num = int(self.cursor.fetchone()[0] + 1)
+			except TypeError:
+				id_num = 0
+
+			self.cursor.execute('SELECT MAX(test_result_id) FROM test_result;')
+			try:
+				id_num = int(self.cursor.fetchone()[0] + 1)
+			except TypeError:
+				id_num = 0
+
+			self.cursor.execute('SELECT MAX(test_result_id) FROM test_result;')
+			try:
+				id_num = int(self.cursor.fetchone()[0] + 1)
+			except TypeError:
+				id_num = 0
+
+			name = child.attrib['name']
+			print("Parsing child %s" % name)
+			testType = re.split('_', child.attrib['classname'])[0]
+			time = child.attrib['time']
+
+
+			date = child.attrib['datestamp']
+			dateString = re.sub(' ', '_', re.sub('[-:]', '', date))
+
+			xml_path = name + "_" + dateString + '.xml'
+			xml_file = open(xml_path, 'w+')
+			xml_name = ET.Element(name, {"name" : name})
+
+			try:
+				mavout = ET.SubElement(xml_name, 'MAVProxy-out')
+				mavout.text = child.find('MAVProxy-out').text
+			except AttributeError:
+				print('Attribute %s does not exist for %s' % ('mavout', name))
+
+			try:
+				jsbout = ET.SubElement(xml_name, 'JSBSim-out')
+				jsbout.text = child.find('JSBSim-out').text
+			except AttributeError:
+				print('Attribute %s does not exist for %s' % ('jsbout', name))
+
+			try:
+				sysout = ET.SubElement(xml_name, 'system-out')
+				sysout.text = child.find('system-out').text
+			except AttributeError:
+				print('Attribute %s does not exist for %s' % ('sysout', name))
+
+			xml_file.write(ET.tostring(xml_name))
+
+			self.cursor.execute('''INSERT INTO test_case 
+				SELECT %(name)s, %(function)s, %(test_type)s
+				WHERE NOT EXISTS (SELECT 1 FROM test_case WHERE test_function=%(function)s);''',
+				{
+				'name' : child.attrib['name'],
+				'function' : child.attrib['name'],
+				'test_type' : testType
+				})
+			self.conn.commit()
+
+			try:
+				self.cursor.execute('SELECT test_case_id FROM test_case WHERE (test_function = %s);', (child.attrib['name'],))
+				caseID = self.cursor.fetchone()[0]
+			except Exception as e:
+				print("Exception in test_case_id lookup: %s" % e)
+				caseID = -1
+
+			self.cursor.execute('''INSERT INTO test_result 
+				VALUES (%(datestamp)s, %(run_num)s, %(run_time)s, %(result)s, 
+					%(test_case_id)s, %(project_id)s, %(sha_list)s, %(xml_path)s);''',
+				{
+				'datestamp':dateString,
+				'run_num':run_num,
+				'run_time':child.attrib['time'],
+				'result':child.attrib['status'],
+				'test_case_id':caseID,
+				'project_id':1,
+				'sha_list':"No sha list yet",
+				'xml_path':xml_path,
+				})
+
+	def finalize(self):
+
+		self.conn.commit()
+		self.conn.close()
+
+def main(argv):
+
+	try:
+		path = sys.argv[1]
+	except IndexError:
+		path = None
+	
+	if not path == None:
+		parser = XMLParser(path)
+	else: 
+		parser = XMLParser()
+	parser.parse()
+	parser.finalize()
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
